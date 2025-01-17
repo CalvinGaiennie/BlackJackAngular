60f88b3d5fece4efa646731f665784b1dd764e01
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-16 22:46:46 -0600

    rewatched a couple lectures from the angular course and applied lessons here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SurveyComponent } from './survey/survey.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SurveyComponent],
+  imports: [SurveyComponent, FormsModule],
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
-
+import { Appointment } from '../models/appointment';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
   styleUrl: './survey.component.css',
+  imports: [FormsModule],
 })
 export class SurveyComponent {
   @Input() message: string = '';
+
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  addAppointment() {
+    console.log(this.newAppointmentTitle + this.newAppointmentDate);
+  }
 }
