ec263932739ff825302b620580fcf09c8e027b97
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-26 21:13:09 -0600

    cleaning up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { QuizPageComponent } from './quiz-page/quiz-page.component';
-import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'Quiz', component: QuizPageComponent },
+  { path: 'Summary', component: QuizPageComponent },
 ];
